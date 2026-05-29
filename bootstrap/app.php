c99b8458ec821028159081226b7c395e6058abeb
Cyber Lord <braxtonnzenong@gmail.com>
2026-05-29 21:34:10 +0100

    security: restrict wildcard trusted proxies to local environment in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
+        if (app()->isLocal()) {
+            $middleware->trustProxies(at: '*');
+        }
 
         $middleware->web(append: [
             HandleInertiaRequests::class,
